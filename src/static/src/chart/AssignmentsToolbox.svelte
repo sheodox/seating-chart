2b13abf7825d26f98bd0496ac6f192d2ac93e745
sheodox <imagamer27@msn.com>
2022-09-10 20:55:39 -0500

    Put the assigned and unassigned guests in two tabs
    With a long guest list this gets super long to scroll past.

--- src/static/src/chart/AssignmentsToolbox.svelte
+++ src/static/src/chart/AssignmentsToolbox.svelte
@@ -1,12 +1,28 @@
 <h1 class="my-0">Guests</h1>
 <div class="f-column gap-6">
-	<AssignmentGuestList guests={unassigned} listTitle="Unassigned" showTable={false} />
-	<AssignmentGuestList guests={assigned} listTitle="Assigned" showTable />
+	<TabList {tabs} bind:selectedTab />
+	<Tab {selectedTab} tabId="unassigned">
+		<AssignmentGuestList guests={unassigned} listTitle="Unassigned" showTable={false} />
+	</Tab>
+	<Tab {selectedTab} tabId="assigned">
+		<AssignmentGuestList guests={assigned} listTitle="Assigned" showTable />
+	</Tab>
 </div>
 
 <script lang="ts">
+	import { TabList, Tab } from 'sheodox-ui';
 	import AssignmentGuestList from './AssignmentGuestList.svelte';
 	import { guests } from '../stores/guests';
+
+	let selectedTab = 'unassigned';
+
+	const tabs = [
+		{
+			id: 'unassigned',
+			title: 'Unassigned',
+		},
+		{ id: 'assigned', title: 'Assigned' },
+	];
 
 	$: unassigned = $guests.filter((g) => g.going && !g.tableId);
 	$: assigned = $guests.filter((g) => g.going && !!g.tableId);
